2051f4240b8b6c62568a215e3746921bb5cab32d
yogesh <level1cr3@gmail.com>
2025-09-06 10:41:03 +0000

    preventing default behaviour of form submission

--- build-tool-vite/src/App.tsx
+++ build-tool-vite/src/App.tsx
@@ -1,7 +1,23 @@
 function App() {
   return (
     <>
-      <form action="/path/to/server">
+      <form
+        action="/path/to/server"
+        method="POST"
+        encType="multipart/form-data"
+        onSubmit={(event) => {
+          event.preventDefault();
+          // once we add onSubmit and preventDefault(), then we don't have to worry about other attributes of form like
+          // action, method, encType. Because now browser is not making the request we would be doing that using fetch or axios and there we would mention method type and encryption etc.
+
+          const formdata = new FormData(event.currentTarget);
+          console.log({
+            form: event.currentTarget,
+            formdata,
+            entries: Object.fromEntries(formdata),
+          });
+        }}
+      >
         <div>
           <label htmlFor="usernameInput">Username: </label>
           <input id="usernameInput" name="username" type="text" />
