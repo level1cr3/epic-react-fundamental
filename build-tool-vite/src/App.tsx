1f94dade6abe1c98897cee9147c2fd448d267d1e
yogesh <level1cr3@gmail.com>
2025-09-06 03:01:37 +0000

    Forms input types

--- build-tool-vite/src/App.tsx
+++ build-tool-vite/src/App.tsx
@@ -1,9 +1,31 @@
 function App() {
   return (
     <>
-      <form action="">
-        <label htmlFor="usernameInput">Username: </label>
-        <input id="usernameInput" name="username" type="text" />
+      <form action="/path/to/server">
+        <div>
+          <label htmlFor="usernameInput">Username: </label>
+          <input id="usernameInput" name="username" type="text" />
+        </div>
+        <div>
+          <label htmlFor="passwordInput">Password: </label>
+          <input id="passwordInput" name="password" type="password" />
+        </div>
+        <div>
+          <label htmlFor="ageInput">Age: </label>
+          <input id="ageInput" name="age" type="number" />
+        </div>
+        <div>
+          <label htmlFor="fileInput">photos: </label>
+          <input id="fileInput" name="file" type="file" accept="image/*" />
+        </div>
+        <div>
+          <label htmlFor="colorInput">FavoriteColor: </label>
+          <input id="colorInput" name="color" type="color" />
+        </div>
+        <div>
+          <label htmlFor="dateInput">Start Date: </label>
+          <input id="dateInput" name="date" type="date" />
+        </div>
         <div>
           <button type="submit">Submit</button>
         </div>
