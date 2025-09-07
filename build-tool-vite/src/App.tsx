4c467e293ee7c742aebef4d6b82ddbb81440a16c
yogesh <level1cr3@gmail.com>
2025-09-07 03:01:24 +0000

    Form various Input fields

--- build-tool-vite/src/App.tsx
+++ build-tool-vite/src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   const logFormData = (formData: FormData) => {
-    console.log(formData);
+    console.log(Object.fromEntries(formData));
   };
 
   return (
@@ -9,6 +9,21 @@ function App() {
         action={logFormData}
         // in react we can pass the function to the action property of form. and it will prevent the default form submission behavior for us and also give us the form data.
       >
+        <input type="hidden" name="id" defaultValue="7" />
+        <div>
+          <label htmlFor="accountTypeDropdown">Account Type: </label>
+          <select
+            name="accountType"
+            id="accountTypeDropdown"
+            defaultValue="student"
+          >
+            <option value="">--Please select an option--</option>
+            <option value="admin">Admin</option>
+            <option value="teacher">Teacher</option>
+            <option value="parent">Parent</option>
+            <option value="student">Student</option>
+          </select>
+        </div>
         <div>
           <label htmlFor="usernameInput">Username: </label>
           <input id="usernameInput" name="username" type="text" />
@@ -19,7 +34,14 @@ function App() {
         </div>
         <div>
           <label htmlFor="ageInput">Age: </label>
-          <input id="ageInput" name="age" type="number" />
+          <input
+            id="ageInput"
+            name="age"
+            type="number"
+            min="0"
+            max="200"
+            defaultValue="18"
+          />
         </div>
         <div>
           <label htmlFor="fileInput">photos: </label>
@@ -27,11 +49,60 @@ function App() {
         </div>
         <div>
           <label htmlFor="colorInput">FavoriteColor: </label>
-          <input id="colorInput" name="color" type="color" />
+          <input
+            id="colorInput"
+            name="color"
+            type="color"
+            defaultValue="#002E5D"
+          />
+        </div>
+
+        <div>
+          <fieldset>
+            <legend>Visibility:</legend>
+
+            <label htmlFor="publicRadio">
+              <input
+                type="radio"
+                name="visibility"
+                value="public"
+                defaultChecked
+                id="publicRadio"
+              />
+              Public
+            </label>
+
+            <label htmlFor="privateRadio">
+              <input
+                type="radio"
+                name="visibility"
+                value="private"
+                id="privateRadio"
+              />
+              Private
+            </label>
+          </fieldset>
+        </div>
+
+        <div>
+          <label htmlFor="checkboxInput">
+            <input
+              id="checkboxInput"
+              name="checkbox"
+              type="checkbox"
+              defaultChecked
+            />
+            Waiver Signed
+          </label>
         </div>
         <div>
           <label htmlFor="dateInput">Start Date: </label>
-          <input id="dateInput" name="date" type="date" />
+          <input
+            id="dateInput"
+            name="date"
+            type="date"
+            defaultValue={new Date().toISOString().slice(0, 10)}
+          />
         </div>
         <div>
           <button type="submit">Submit</button>
@@ -42,3 +113,21 @@ function App() {
 }
 
 export default App;
+
+// you think of attributes and properties like this
+// attributes are the initial values or configurations. where as Properties are dynamic values
+
+/*Default values
+Because react allows us to be dynamic.
+
+If we set the value prop in input react will make sure it never changes from that value.
+So that way we can programmatically change that value.
+For that reason if we set value prop in input react will not allow the user to change it
+
+But if you want to initialize input to certain value that can be changed later we
+would use defaultValue
+
+defaultValue is react specific not the web.
+
+
+*/
